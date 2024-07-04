b8a090671732223e28a6f094ddb76f05f7338017
Bisha <bisha.mail.drop@gmail.com>
2024-07-04 19:04:34 +0400

    feat: 10001st prime

---
+++ archive/problem_7.py
@@ -0,0 +1,20 @@
+# By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+# What is the 10001st prime number?
+
+
+n = 2
+primes = []
+
+def is_prime(n):
+    divisibles = []
+    for i in range(1, n + 1):
+        if (n % i == 0):
+            divisibles.append(i)
+    return len(divisibles) <= 2
+
+while len(primes) < 10001:
+    if (is_prime(n)):
+        primes.append(n)
+    n = n + 1
+
+print(primes)
